c6397ceae424be000963538db5fdcba42ffa9b1d
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-26 00:28:24 +0300

    added findByName method

--- src/main/java/com/workintech/repository/ActorRepository.java
+++ src/main/java/com/workintech/repository/ActorRepository.java
@@ -3,5 +3,8 @@ package com.workintech.repository;
 import com.workintech.entity.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+    Optional<Actor> findByFirstNameAndLastName(String firstName, String lastName);
 }
--- src/main/java/com/workintech/repository/MovieRepository.java
+++ src/main/java/com/workintech/repository/MovieRepository.java
@@ -3,5 +3,8 @@ package com.workintech.repository;
 import com.workintech.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+    Optional<Movie> findByName(String name);
 }

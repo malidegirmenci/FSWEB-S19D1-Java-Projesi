c84c1ec45fed32c1d9e62bd2b354de7632dd5e3d
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-25 16:33:12 +0300

    created dto converter

---
+++ src/main/java/com/workintech/converter/DtoConverter.java
@@ -0,0 +1,52 @@
+package com.workintech.converter;
+
+import com.workintech.dto.ActorResponse;
+import com.workintech.dto.MovieResponse;
+import com.workintech.entity.Actor;
+import com.workintech.entity.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DtoConverter {
+    public static MovieResponse convertToMovieResponse(Movie movie){
+        return new MovieResponse(
+                movie.getName(),
+                movie.getDirectorName(),
+                movie.getRating(),
+                movie.getReleaseDate(),
+                movie.getActorList() == null ? new ArrayList<>() : convertToActorResponseList(movie.getActorList())
+        );
+    }
+    public static List<MovieResponse> convertToMovieResponseList(List<Movie> movieList){
+        List<MovieResponse> responses = new ArrayList<>();
+        movieList.forEach(movie -> responses.add(new MovieResponse(
+                movie.getName(),
+                movie.getDirectorName(),
+                movie.getRating(),
+                movie.getReleaseDate(),
+                movie.getActorList() == null ? new ArrayList<>() : convertToActorResponseList(movie.getActorList())
+        )));
+        return responses;
+    }
+    public static ActorResponse convertToActorResponse(Actor actor){
+        return new ActorResponse(
+                actor.getFirstName(),
+                actor.getLastName(),
+                actor.getGender(),
+                actor.getBirthDate()
+
+        );
+    }
+    public static List<ActorResponse> convertToActorResponseList(List<Actor> actorList){
+        List<ActorResponse> responses = new ArrayList<>();
+        actorList.forEach(actor -> responses.add(new ActorResponse(
+                actor.getFirstName(),
+                actor.getLastName(),
+                actor.getGender(),
+                actor.getBirthDate()
+
+        )));
+        return responses;
+    }
+}

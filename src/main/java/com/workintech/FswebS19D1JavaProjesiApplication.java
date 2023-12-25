5f40cef7be335f8faffa189691c8a25f25d22d8b
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-25 13:48:41 +0300

    init spring boot

---
+++ src/main/java/com/workintech/FswebS19D1JavaProjesiApplication.java
@@ -0,0 +1,13 @@
+package com.workintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FswebS19D1JavaProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FswebS19D1JavaProjesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/FswebS19D1JavaProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FswebS19D1JavaProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

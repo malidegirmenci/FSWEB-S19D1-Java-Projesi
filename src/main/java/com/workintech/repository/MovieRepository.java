df11cf856836fc4985ffec014325b9eb3e0314fe
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-25 16:31:36 +0300

    create repository layer

---
+++ src/main/java/com/workintech/repository/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.workintech.repository;
+
+import com.workintech.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+}

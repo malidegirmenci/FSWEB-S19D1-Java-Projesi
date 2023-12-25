fffe2653e086a1c1eb3c0d372ff9f80200be7425
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-26 00:29:00 +0300

    removed JsonIgnore annotation

--- src/main/java/com/workintech/entity/Movie.java
+++ src/main/java/com/workintech/entity/Movie.java
@@ -1,5 +1,6 @@
 package com.workintech.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -51,5 +52,4 @@ public class Movie {
         }
         actorList.add(actor);
     }
-
 }
